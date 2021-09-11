64f870426e76d17c6752bb6f46b2a4ac31275fc7
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-09-11 15:57:39 -0700

    Updated movies array and added access to myFlixDB

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
-
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
@@ -9,28 +9,21 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          "_id": { "$oid": "610d6f14388fb37c75e1308d" },
-          "title": "Iron Man",
-          "description": "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
-          "imagepath": "ironMan.jpg"
-        },
-        {
-          "_id": { "$oid": "610d797d388fb37c75e13095" },
-          "title": "The Help",
-          "description": "Updated Version: An aspiring author during the civil rights movement of the 1960s decides to write a book detailing the African American maids' point of view on the white families for which they work, and the hardships they go through on a daily basis.",
-          "imagepath": "theHelp.jpg"
-        },
-        {
-          "_id": { "$oid": "610d7996388fb37c75e13096" },
-          "title": "Shindler's List",
-          "description": "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
-          "imagepath": "schindlersList.jpg"
-        }
-      ],
+      movies: [],
       selectedMovie: null
     }
+  }
+
+  componentDidMount() {
+    axios.get('https://myflixdd.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -43,7 +36,7 @@ export class MainView extends React.Component {
     const { movies, selectedMovie } = this.state;
 
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
