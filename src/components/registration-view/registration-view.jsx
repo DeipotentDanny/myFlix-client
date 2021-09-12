21a6cabb843ff5ffc908ec5cbe6dd583d5c834df
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-09-11 17:51:27 -0700

    Created views for login and registration

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [birth_date, setBirthDate] = useState('');
+
+  const handleSubmit = () => {
+    e.preventDefault();
+    console.log(username, password, email, birth_date);
+    props.onRegister(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Birth Date:
+        <input type="date" value={birth_date} onChange={e => setBirthDate(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Register</button>
+    </form>
+  );
+}
+
+/* Registration Structure
+  username: "ExampleUsername123"
+  password: "PassEXpassword1!"
+  email: "example.email@email.com"
+  birth_date: "1997/02/31" (ex. date in ISO)
+*/
