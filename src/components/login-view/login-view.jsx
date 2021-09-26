8974fd08d52cefe200d7d786908894f74a9e3fa4
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-09-26 10:37:40 -0700

    Squashed commit of the following:
    commit 0c2a809637c5a2a80754f607521f87efebf16c4b
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Mon Sep 20 23:05:44 2021 -0700
    
        Update registration-view.jsx
    
    commit d7fc9bb32f7cecbc7c896fd92cf98fc352351ba0
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Mon Sep 20 23:05:39 2021 -0700
    
        Update login-view.jsx
    
    commit b15d40f6bcfd18c74e59c0816d0b7eeb8dfc85c3
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Mon Sep 20 23:05:36 2021 -0700
    
        Updated dist files
    
    commit ad8bc411940b497c2f05908d23ee0fec7628ca9c
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Sun Sep 19 23:01:46 2021 -0700
    
        Update registration-view.jsx
    
    commit db0008eb41d2bb673cf817e4c463874075f93245
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Sun Sep 19 23:01:43 2021 -0700
    
        Update login-view.jsx
    
    commit 9c15b9654f3cbe521d6d9fdadd18bd8aa253f31c
    Author: DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
    Date:   Sun Sep 19 23:01:40 2021 -0700
    
        Updated dist files

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from "prop-types";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -16,17 +18,24 @@ export function LoginView(props) {
   };
 
   return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-    </Form>
+    <Row className="d-flex mx-auto mt-5 justify-content-center">
+      <Col xs={12} md={8} lg={6} className="d-flex mx-auto">
+        <h2 className="text-center mt-5 text-light">Please Login</h2>
+        <Row>
+          <Form>
+            <Form.Group controlId="formUsername">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+            <Form.Group controlId="formPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+          </Form>
+        </Row>
+      </Col>
+    </Row>
   );
 }
 
