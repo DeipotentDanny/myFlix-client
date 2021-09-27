4f25c67bd874c8e49500fc735053eda3ddba3d89
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-09-26 18:57:40 -0700

    Created genre-view.jsx

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './genre-view.scss'
+import Button from 'react-bootstrap/Button';
+
+export class GenreView extends React.Component {
+
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="genre-view">
+        <div className="genre-name">
+          <span className="value">{movie.genre.name}</span>
+        </div>
+        <div className="genre-description">
+          <span className="label">Desc: </span>
+          <span className="value">{movie.genre.description}</span>
+        </div>
+        <Button onClick={() => { onBackClick(null); }}>Back</Button>
+      </div>
+    );
+  }
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired
+};
