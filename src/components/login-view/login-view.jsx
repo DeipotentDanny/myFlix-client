0414e4d3ea5cdabf071d598af639af3bb447d02a
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-09-27 08:51:01 -0700

    Update login-view.jsx
    Added form validation

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,28 +1,55 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
+import './login-view.scss';
+
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const [usernameError, setUsernameError] = useState({});
+  const [passwordError, setPasswordError] = useState({});
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('myflixdd.herokuapp.com', {
-      username: username,
-      password: password
-    })
-      .then(response => {
-        const data = response.data;
-        props.onLoggedIn(data);
+    let setisValid = formValidation();
+    if (setisValid) {
+      axios.post('https://myflixdd.herokuapp.com', {
+        username: username,
+        password: password
       })
-      .catch(e => {
-        console.log('no such user')
-      });
+        .then(response => {
+          const data = response.data;
+          props.onLoggedIn(data);
+        })
+        .catch(e => {
+          console.log('no such user')
+        });
+    };
+  }
+
+  const formValidation = () => {
+    let usernameError = {};
+    let passwordError = {};
+    let isValid = true;
+
+    if (username.trim().length < 4) {
+      usernameError.usernameShort = "Username incorrect. Use at least 4 characters.";
+      isValid = false;
+    }
+    if (password.trim().length < 5) {
+      passwordError.passwordMissing = "Password incorrect. Use at least 5 characters.";
+      isValid = false;
+    }
+    setUsernameError(usernameError);
+    setPasswordError(passwordError);
+    return isValid;
   };
 
   return (
@@ -30,17 +57,37 @@ export function LoginView(props) {
       <Col xs={12} md={8} lg={6} className="d-flex mx-auto">
         <h2 className="text-center mt-5 text-light">Please Login</h2>
         <Row>
+
           <Form>
+
             <Form.Group controlId="formUsername">
               <Form.Label>Username:</Form.Label>
-              <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+              <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+              {Object.keys(usernameError).map((key) => {
+                return (
+                  <div key={key}>
+                    {usernameError[key]}
+                  </div>
+                );
+              })}
             </Form.Group>
+
             <Form.Group controlId="formPassword">
               <Form.Label>Password:</Form.Label>
-              <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+              <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+              {Object.keys(passwordError).map((key) => {
+                return (
+                  <div key={key}>
+                    {passwordError[key]}
+                  </div>
+                );
+              })}
             </Form.Group>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Sign In</Button>
+
           </Form>
+
         </Row>
       </Col>
     </Row>
