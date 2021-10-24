d65553ad4a5585834c8c0c9dfddc8593d81c2bef
DeipotentDanny <79887944+DeipotentDanny@users.noreply.github.com>
2021-10-24 08:58:37 -0700

    Update genre-view.jsx
    updated variable movie to genre

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -6,16 +6,16 @@ import Button from 'react-bootstrap/Button';
 export class GenreView extends React.Component {
 
   render() {
-    const { movie, onBackClick } = this.props;
+    const { genre, onBackClick } = this.props;
 
     return (
       <div className="genre-view">
         <div className="genre-name">
-          <span className="value">{movie.genre.name}</span>
+          <span className="value">{genre.name}</span>
         </div>
         <div className="genre-description">
           <span className="label">Desc: </span>
-          <span className="value">{movie.genre.description}</span>
+          <span className="value">{genre.description}</span>
         </div>
         <Button onClick={() => { onBackClick(null); }}>Back</Button>
       </div>
